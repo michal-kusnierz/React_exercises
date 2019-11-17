2b5afc4bf9f7e6658d6f95c07afe545bc42903fe
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-18 00:21:40 +0100

    create props on Bike's name and url

--- src/App.js
+++ src/App.js
@@ -9,7 +9,11 @@ function App() {
     <div className="App">
       <Hero />
       <Content />
-      <Bike />
+      <Bike 
+        name="retro"
+        avatarUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQi2N3PhJYSXCcKgEBLopgCMsodhw8R4WS0BGIf716Ysw65j7Ds&s"/>
+      <Bike name="ordinary bike..."/>
+      <Bike name="is it?"/>
     </div>
   );
 }
--- src/components/Bike.js
+++ src/components/Bike.js
@@ -1,22 +1,28 @@
 import React from 'react';
 
-function BikeName(){
-  return <h1>Bike?</h1>
+function BikeName(props){
+  return <h1>{props.nameOfBike}</h1>
 }
 
 const bikeSrc=
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvZPwgpXsplbCYJR2IaRo8sg4UxzN6OMcgWJt5BQ9B_3yZvW7SIg&s";
 
-  const BikeImage = () => {
-  return <img src={bikeSrc} />
+  const BikeImage = props => {
+  const { url } = props;
+
+  if (url){
+    return <img src={url} />
+  } else {
+    return null
+  }
 };
 
 const showName = true;
 
-const Bike = () => (
+const Bike = props => (
   <div>
-    <BikeImage />
-    {showName && <BikeName />}
+    <BikeImage url={props.avatarUrl}/>
+    {showName && <BikeName nameOfBike={props.name} />}
   </div>
 );
 
