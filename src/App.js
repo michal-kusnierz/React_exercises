d1b3777c7d254643c7fc71109f4e899fece6db75
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-17 23:49:08 +0100

    replace src with variable

--- src/App.js
+++ src/App.js
@@ -33,10 +33,11 @@ function BikeName(){
   return <h1>Bike?</h1>
 }
 
-function BikeImage (){
-  return (
-    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvZPwgpXsplbCYJR2IaRo8sg4UxzN6OMcgWJt5BQ9B_3yZvW7SIg&s"/>
-  );
+const bikeSrc=
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvZPwgpXsplbCYJR2IaRo8sg4UxzN6OMcgWJt5BQ9B_3yZvW7SIg&s";
+
+  const BikeImage = () => {
+  return <img src={bikeSrc} />
 };
 
 const showName = true;
