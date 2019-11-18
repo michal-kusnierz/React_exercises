d4314a0852bf2ca9b658e67fe6aa2c66589e0446
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-18 01:58:15 +0100

    Switch

--- src/Root.js
+++ src/Root.js
@@ -1,37 +1,40 @@
-
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import App from "./App";
 
 const Root = () => {
   return (
     <Router>
       <div>
-        <Route exact path="/" component={() => <h1>Home!</h1>} />
-        <Route exact path="/abc" component={() => <h1>/abc</h1>} />
-        <Route exact path="/abc/" component={() => <h1>/abc/</h1>} />
-        <Route exact path="/ABC/" component={() => <h1>/ABC/</h1>} />
-        <Route exact path="/abc/def" component={() => <h1>ABC/DEF</h1>} />
-        <Route
-          path="/users/:id"
-          component={props => {
-            console.log(props.match.params.id);
-            return <h1>Users {props.match.params.id}</h1>;
-          }}
-        />
-        <Route
-          path="/users/:id/:name"
-          component={props => {
-            const { id, name } = props.match.params;
-            return (
-              <h1>
-                Users {id} - {name}
-              </h1>
-            );
-          }}
+      <Switch>
+          <Route exact path="/" component={App} />
+          <Route exact path="/abc" component={() => <h1>/abc</h1>} />
+          <Route exact path="/abc/" component={() => <h1>/abc/</h1>} />
+          <Route exact path="/ABC/" component={() => <h1>/ABC/</h1>} />
+          <Route exact path="/abc/def" component={() => <h1>ABC/DEF</h1>} />
+          <Route
+            path="/users/:id"
+            component={props => {
+              console.log(props.match.params.id);
+              return <h1>Users {props.match.params.id}</h1>;
+            }}
+          />
+          <Route
+            path="/users/:id/:name"
+            component={props => {
+              const { id, name } = props.match.params;
+              return (
+                <h1>
+                  Users {id} - {name}
+                </h1>
+              );
+            }}
+          />
+        </Switch>
         />
       </div>
     </Router>
   );
 };
 
-export default Root
+export default Root;
