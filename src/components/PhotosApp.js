5aa660365bb7baa181dfc8dbbb23db47bf63c90b
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-23 20:20:46 +0100

    fast PhotosApp implementation

--- src/components/PhotosApp.js
+++ src/components/PhotosApp.js
@@ -1,8 +1,81 @@
 import React from "react";
+import { Header, Card, Icon, Image, Menu, Segment, Sidebar } from "semantic-ui-react";
+import { photos } from "../data";
 
+const Photo = props => {
+  const {
+    imageUrl = "https://images.pexels.com/photos/948331/pexels-photo-948331.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    description = "Unknown",
+    createdAt = "Unknown"
+  } = props;
+
+  return (
+    <Card>
+      <Image src={imageUrl} wrapped ui={false} />
+      <Icon name="close" size="medium" />
+      <Card.Content>
+        <Card.Header>{description}</Card.Header>
+        <Card.Meta>
+          <span className="date">{createdAt}</span>
+        </Card.Meta>
+      </Card.Content>
+    </Card>
+  );
+};
 class PhotosApp extends React.Component {
+  state = {
+    photos: photos
+  };
+
+  deletePhoto = id => {
+    const newPhotos = this.state.photos.filter(photo => photo.id !== id);
+
+    this.setState({
+      photos: newPhotos
+    });
+  };
+  
   render() {
-    return <h1>Photos app</h1>;
+    return (
+      <div style={{ height: "100vh" }}>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation="push"
+            icon="labeled"
+            inverted
+            onHide={() => {}}
+            vertical
+            visible={true}
+            width="wide"
+          >
+            <Menu.Item as="a">Home</Menu.Item>
+            <Menu.Item as="a">Games</Menu.Item>
+            <Menu.Item as="a">Channels</Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Card.Group>
+                {this.state.photos.map(photo => (
+                  <div
+                    key={photo.id}
+                    onClick={() => this.deletePhoto(photo.id)}
+                  >
+                    <Photo
+                      imageUrl={photo.image}
+                      description={photo.desc}
+                      createdAt={photo.date}
+                    />
+                  </div>
+                ))}
+        
+      </Card.Group>
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </div>
+    );
   }
 }
 
--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-const albums = [
+export const albums = [
   {
     id: 0,
     name: "Rodzinne",
@@ -6,26 +6,27 @@ const albums = [
   }
 ];
 
-const photos = [
+export const photos = [
   {
     id: 0,
-    desc: "Zdjecie z wakacji",
+    desc: "busy",
     date: "12-12-2012",
     image:
-      "https://images.pexels.com/photos/2658457/pexels-photo-2658457.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuvq7b8lWip5nhZJpGJVoaRrLjywsF9LzwaEN-dd0ZdcdCOxXz&s"
   },
   {
     id: 1,
-    desc: "Dziwne zdjecie",
+    desc: "yes",
     date: "21-07-2012",
     image:
-      "https://images.pexels.com/photos/2538412/pexels-photo-2538412.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqUDf-78BBHiVbe-HZG2WBbsKCFNZN9EcmxM5-KJfxIc582ABt&s"
+
   },
   {
     id: 2,
-    desc: "Selfie",
+    desc: "some desc",
     date: "01-12-2002",
     image:
-      "https://images.pexels.com/photos/3014019/pexels-photo-3014019.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJNp1Hd85thypZImArX9r8vKHLUdC1XvGNdqJ6vUKit1DFLcGJ&s"
   }
 ];
