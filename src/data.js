83560d88ab1a2a686b1837fc3156f3e41593b9f9
michal-kusnierz <kusnierz.michal@gmail.com>
2019-11-23 19:57:01 +0100

    added sample data with albums and photos

---
+++ src/data.js
@@ -0,0 +1,31 @@
+const albums = [
+  {
+    id: 0,
+    name: "Rodzinne",
+    photos: [0]
+  }
+];
+
+const photos = [
+  {
+    id: 0,
+    desc: "Zdjecie z wakacji",
+    date: "12-12-2012",
+    image:
+      "https://images.pexels.com/photos/2658457/pexels-photo-2658457.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 1,
+    desc: "Dziwne zdjecie",
+    date: "21-07-2012",
+    image:
+      "https://images.pexels.com/photos/2538412/pexels-photo-2538412.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  },
+  {
+    id: 2,
+    desc: "Selfie",
+    date: "01-12-2002",
+    image:
+      "https://images.pexels.com/photos/3014019/pexels-photo-3014019.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+  }
+];
